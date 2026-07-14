0416736703d6d4507a23e6a1fff546aac1a41b1d
Vipul Gupta <vipulgupta92998@gmail.com>
2026-07-15 00:40:09 +0530

    firebase for frontend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div>
-      hello 
+    <div className='w-full h-screen bg-black text-white flex items-center justify-center'>
+   <button className='w-50 h-20 text-black bg-white'>
+    Continue with google
+   </button>
     </div>
   );
 }
---
+++ frontend/utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB5qLgv7HuOFlup1OBjoNnrIefYf43Z7EY",
+  authDomain: "blackeagle-2acfa.firebaseapp.com",
+  projectId: "blackeagle-2acfa",
+  storageBucket: "blackeagle-2acfa.firebasestorage.app",
+  messagingSenderId: "709772535069",
+  appId: "1:709772535069:web:65bb42889db8dd367eedf4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
