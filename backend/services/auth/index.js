c726ac32e31af8c580ee2dff271bc2463d572900
Vipul Gupta <vipulgupta92998@gmail.com>
2026-07-17 00:42:22 +0530

    api util created

--- backend/services/auth/index.js
+++ backend/services/auth/index.js
@@ -1,11 +1,16 @@
 import express from "express";
 import dotenv from "dotenv"
 import connectDb from "./config/db.js";
+import router from "./routes/auth.route.js";
 dotenv.config();
 
 const port = process.env.PORT
 const app = express()
+
 app.use(express.json())
+
+app.use("/",router)
+
 app.get("/",(req , res)=>{
     res.status(200).json({message:"Auth server running"})
 })
