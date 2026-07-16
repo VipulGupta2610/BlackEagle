4b0af41b4e303ac08db47908b4cbdf38318f0ee5
Vipul Gupta <vipulgupta92998@gmail.com>
2026-07-17 00:30:34 +0530

    user controller setup

--- backend/services/auth/controllers/auth.controller.js
+++ backend/services/auth/controllers/auth.controller.js
@@ -1,10 +1,22 @@
 import {getAuth} from "firebase-admin/auth"
 import app from "../config/firebase.js"
+import User from "../models/user.model.js";
 
 export const login = async (req , res)=>{
     try {
         const {token} = req.body;
-        const decoded = getAuth(app).verifyIdToken(token);
+        const decoded = await getAuth(app).verifyIdToken(token);
+        const user = await User.findOne({
+            firebaseUID:decoded.uid
+        });
+        if (!user){
+            user = await User.create({
+                firebaseUID:decoded.uid,
+                name:decoded.name,
+                email:decoded.email,
+                avatar:decoded.picture
+            })
+        }
     } catch (error) {
         
     }
