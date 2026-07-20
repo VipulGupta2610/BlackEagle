5bed3ad3b73784067bebc46cacac9a9d70d731e4
Vipul Gupta <vipulgupta92998@gmail.com>
2026-07-21 00:46:23 +0530

    backend workign

--- backend/gateway/middleware/auth.midlleware.js
+++ backend/gateway/middleware/auth.midlleware.js
@@ -1,3 +1,17 @@
-const protect = async (req , res,next)=>{
+import redis from "../../shared/redis"
 
+const protect = async (req, res, next) => {
+    try {
+        const sessionId = req.Cookies?.session
+        if (!sessionId){
+           return res.status(400).json({message:"Uauthorized"})
+        }
+       const session =  await redis.get(`session-${sessionId}`)
+       if (!session){
+        return res.status(500).json({message:"Session expired. Login again"})
+       }
+       req.
+    } catch (error) {
+
+    }
 }
