e0e0c7a14f6283650f2c7651af5c7a213e978483
Vipul Gupta <vipulgupta92998@gmail.com>
2026-07-21 00:22:45 +0530

    login ui ready

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,12 @@
-import { signInWithPopup } from 'firebase/auth';
-import React from 'react';
-import { auth, googleProvider } from '../utils/firebase.js';
-import api from '../utils/axios.js';
+import React from 'react'
+import Home from './pages/Home'
 
-const App = () => {
-
-  const handleLogin=async (token)=>{
-    try {
-      const {data} = await api.post("/auth/login",{token})
-    } catch (error) {
-      console.log("Error at handleLogin")
-      console.log(error)
-    }
-  }
-
-  const googleLogin = async ()=>{
-  const data =  await signInWithPopup(auth,googleProvider)
-  const token = await data.user.getIdToken()
-  await handleLogin(token)
-  }
-
+function App() {
   return (
-    <div className='w-full h-screen bg-black text-white flex items-center justify-center'>
-   <button onClick={googleLogin} className='w-50 h-20 text-black bg-white'>
-    Continue with google
-   </button>
-    </div>
-  );
+    <>
+      <Home/>
+    </>
+  )
 }
 
-export default App;
+export default App
