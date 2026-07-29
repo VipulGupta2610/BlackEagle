2e7b98321ac33922d9cbc940909d52b863435db2
Vipul Gupta <vipulgupta92998@gmail.com>
2026-07-30 00:19:38 +0530

    conversation model created

--- backend/services/chat/models/conversation.model.js
+++ backend/services/chat/models/conversation.model.js
@@ -1,5 +1,14 @@
 import mongoose from "mongoose";
 
 const conversationSchema = new mongoose.Schema({
-    
+    ttile:{
+        type:String,
+        default:"New chat"
+    },
+    userId:{
+        type:String
+    }
 },{timestamps:true})
+
+const Conversation = mongoose.model("Conversation" , conversationSchema)
+export default Conversation
