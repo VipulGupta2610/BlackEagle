fdb6e67a40c97c4663ab9f11dc2562b9d7ba9cfd
Vipul Gupta <vipulgupta92998@gmail.com>
2026-07-31 14:24:34 +0530

    chat service connneected

--- backend/gateway/index.js
+++ backend/gateway/index.js
@@ -15,6 +15,7 @@ app.use(cors({
 }))
 app.use(cookieParser())
 app.use("/api/auth" , proxy((process.env.AUTH_SERVICE)))
+app.use("/api/chat" , proxy((process.env.CHAT_SERVICE)))
 app.get("/api/me",protect,getCurrentUser)
 app.get("/",(req , res)=>{
     res.status(200).json({message:"Gateway server running"})
---
+++ backend/services/chat/controllers/chat.controller.js
@@ -0,0 +1,7 @@
+export const createConversation = async (req , res)=>{
+    try {
+        
+    } catch (error) {
+        
+    }
+}
