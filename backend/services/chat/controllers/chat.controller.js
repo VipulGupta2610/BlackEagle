10a3680c40d8daa016e1a9574e6244b2f3c04c89
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-01 12:08:46 +0530

    routes for all controllers done

--- backend/services/chat/controllers/chat.controller.js
+++ backend/services/chat/controllers/chat.controller.js
@@ -53,9 +53,8 @@ export const saveMessage = async (req , res)=>{
 }
 export const getMessage = async (req , res)=>{
     try {
-        const {conversationId} = req.body;
         const messages= await Message.find({
-            conversationId
+            conversationId:req.params.conversationId
         }).sort({createdAt:-1})
         return res.status(200).json(messages)
     } catch (error) {
