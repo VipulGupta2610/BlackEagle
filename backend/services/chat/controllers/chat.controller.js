e951d6576beae46e9aeb7921bc49f0c1f4bb74b3
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-01 11:49:45 +0530

    conversation model ready

--- backend/services/chat/controllers/chat.controller.js
+++ backend/services/chat/controllers/chat.controller.js
@@ -1,7 +1,14 @@
+import Conversation from "../models/conversation.model.js"
+
 export const createConversation = async (req , res)=>{
     try {
-        
+        const userId = req.headers["x-user-id"]
+        console.log(userId)
+        const conversation = await Conversation.create({
+            userId:userId
+        })
+        return res.status(200).json(conversation)
     } catch (error) {
-        
+        return res.status(500).json({message:"Create conversation error" , error})
     }
 }
