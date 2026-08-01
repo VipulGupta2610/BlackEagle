9b09689f512979739feca7009787510911099458
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-01 11:52:06 +0530

    Get converstion controller ready

--- backend/services/chat/controllers/chat.controller.js
+++ backend/services/chat/controllers/chat.controller.js
@@ -12,3 +12,16 @@ export const createConversation = async (req , res)=>{
         return res.status(500).json({message:"Create conversation error" , error})
     }
 }
+
+export const getConversation = async (req , res)=>{
+    try {
+        const userId = req.headers["x-user-id"]
+        console.log(userId)
+        const conversation = await Conversation.find({
+            userId:userId
+        })
+        return res.status(200).json(conversation)
+    } catch (error) {
+        return res.status(500).json({message:"Get conversation error" , error})
+    }
+}
