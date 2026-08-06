ed3d86d2d63060a0792803fabf6f112c7853fdd9
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-06 22:34:22 +0530

    conditional edges ready

--- backend/services/agent/graph/graph.js
+++ backend/services/agent/graph/graph.js
@@ -6,24 +6,35 @@ import { searchAgent } from "../agents/search.agent.js";
 import { pdfAgent } from "../agents/pdf.agent.js";
 import { pptAgent } from "../agents/ppt.agent.js";
 import { imageGenAgent } from "../agents/imageGen.agnet.js";
+import { codingAgent } from "../agents/coding.agent.js";
 
 const workflow = new StateGraph(agnetState)
 
 workflow.addNode("router" , router)
 workflow.addNode("chat" , chatAgent)
 workflow.addNode("search" , searchAgent)
+workflow.addNode("coding" , codingAgent)
 workflow.addNode("pdf" , pdfAgent)
 workflow.addNode("ppt" , pptAgent)
-workflow.addNode("imageGen" ,  imageGenAgent)
+workflow.addNode("vision" ,  imageGenAgent)
 
 workflow.addEdge("__start__" , router)
 workflow.addConditionalEdges("router" , (state)=>{
     switch (state.agent) {
-        case value:
-            
-            break;
+        case "chat":
+            return "chat";
+        case "search":
+            return "search";
+        case "coding":
+            return "coding";
+        case "pdf":
+            return "pdf";
+        case "ppt":
+            return "ppt";
+        case "vision":
+            return "vision";
     
         default:
-            break;
+            return "llm";
     }
 })
