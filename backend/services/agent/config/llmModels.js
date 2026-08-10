8d710dafb300439d21d091284e64d3f5f1fb8437
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-10 22:37:23 +0530

    gemini model initialized

--- backend/services/agent/config/llmModels.js
+++ backend/services/agent/config/llmModels.js
@@ -1,8 +1,16 @@
 import { ChatGroq } from "@langchain/groq"
+import { ChatGoogleGenerativeAI } from "@langchain/google-genai"
 
 const groq = new ChatGroq({
     model: "openai/gpt-oss-120b",
     temperature: 0,
     maxTokens: undefined,
     maxRetries: 2,
+})
+
+const gemini = new ChatGoogleGenerativeAI({
+    model: "gemini-2.5-pro",
+    temperature: 0,
+    maxRetries: 2,
+    // other params...
 })
