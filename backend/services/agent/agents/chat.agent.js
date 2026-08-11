d6d8f46f498de81564302e7a86bc47e3cefcd021
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-11 23:52:32 +0530

    chatagent ready for testing

--- backend/services/agent/agents/chat.agent.js
+++ backend/services/agent/agents/chat.agent.js
@@ -1,6 +1,17 @@
 import { getModel } from "../config/llmModels"
 
 export const chatAgent = async (state) => {
-    const llm = getModel("chat")
+    const llm = await getModel("chat")
+    const systemprompt = "You are BlackEagleAI , an intelligent AI Assistant"
+    const response = await llm.invoke([{
+        "role": "system",
+        "content": systemprompt,
 
+    },
+    {
+        "role": "human",
+        "content": state.prompt
+    }
+    ])
+    return { ...state, aiResponse: response.content }
 }
