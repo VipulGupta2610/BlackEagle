580a6edf63a70a8f70691a3d5b75f104130e827a
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-12 23:40:06 +0530

    using chat server for agents

--- backend/services/agent/cotrollers/agent.controller.js
+++ backend/services/agent/cotrollers/agent.controller.js
@@ -1,8 +1,14 @@
-export const agent = async (req , res)=>{
+import axios from "axios"
+import {graph} from "../graph/graph.js"
+
+export const agent = async (req, res) => {
     try {
-        const {prompt , conversationId} = req.body;
-        await axios
+        const { prompt, conversationId } = req.body;
+        await axios.post(`${process.env.CHAT_SERVICE}/save-message`, { conversationId: conversationId, role: "user", content: prompt })
+        const result = await graph.invoke({
+            prompt , 
+        })
     } catch (error) {
-        
+
     }
 }
