f9f244bb1deca888a976270d62ae3aeabea7e02d
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-15 13:04:22 +0530

    added unique conversation id

--- backend/services/agent/cotrollers/agent.controller.js
+++ backend/services/agent/cotrollers/agent.controller.js
@@ -6,7 +6,7 @@ export const agent = async (req, res) => {
         const { prompt, conversationId } = req.body;
         await axios.post(`${process.env.CHAT_SERVICE}/save-message`, { conversationId: conversationId, role: "user", content: prompt })
         const result = await graph.invoke({
-            prompt , 
+            prompt , ConversationID
         })
     } catch (error) {
 
