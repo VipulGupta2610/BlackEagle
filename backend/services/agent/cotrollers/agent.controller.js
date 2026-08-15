6d6e5719fd81f3de44234463d03d83a0f6627efd
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-15 13:09:43 +0530

    response sent to frontend

--- backend/services/agent/cotrollers/agent.controller.js
+++ backend/services/agent/cotrollers/agent.controller.js
@@ -6,9 +6,11 @@ export const agent = async (req, res) => {
         const { prompt, conversationId } = req.body;
         await axios.post(`${process.env.CHAT_SERVICE}/save-message`, { conversationId: conversationId, role: "user", content: prompt })
         const result = await graph.invoke({
-            prompt , ConversationID
+            prompt , conversationID
         })
+        const response = result.aiResponse
+        return res.status(200).json(response)
     } catch (error) {
-
+        return res.status(500).json({message:`Agent error ${error}`})
     }
 }
