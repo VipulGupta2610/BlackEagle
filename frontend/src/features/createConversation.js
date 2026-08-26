25e3fc2c49bea703fb5f57573a213db400bced1b
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-26 23:27:18 +0530

    some updates

--- frontend/src/features/createConversation.js
+++ frontend/src/features/createConversation.js
@@ -3,8 +3,9 @@ import api from "../../utils/axios.js"
 export const createConversation = async ()=>{
     try {
         const {data} = await api.get("/api/chat/create-conversation")
-        
+        return data
     } catch (error) {
         console.log(error)
+        return []
     }
 }
--- frontend/src/features/getConversations.js
+++ frontend/src/features/getConversations.js
@@ -3,8 +3,9 @@ import api from "../../utils/axios.js"
 export const getConversations = async ()=>{
     try {
         const {data} = await api.get("/api/chat/get-conversations")
-        
+        return data
     } catch (error) {
         console.log(error)
+        return []
     }
 }
