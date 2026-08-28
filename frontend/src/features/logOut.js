f5f04e9da0a616242b421cb06cf8bbed7121cdee
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-28 22:30:15 +0530

    feature ready

--- frontend/src/features/logOut.js
+++ frontend/src/features/logOut.js
@@ -3,9 +3,10 @@ import api from '../../utils/axios.js'
 
 async function logOut() {
     try {
-        const {data} = await api.get("/api/auth")
+        const {data} = await api.get("/api/auth/logout")
+        console.log(data)
     } catch (error) {
-        
+        console.log(error)
     }
 }
 
