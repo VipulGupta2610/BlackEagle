fc357daae812eb784c4756afa3384f29ce80d6f0
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-29 12:59:28 +0530

    get messages api created

--- backend/services/chat/routes/chat.routes.js
+++ backend/services/chat/routes/chat.routes.js
@@ -7,7 +7,7 @@ router.get("/create-conversation" , createConversation)
 router.get("/get-conversation" , getConversation)
 router.post("/update-conversation" , updateConversation)
 router.post("/save-message" , saveMessage)
-router.get("/get-message/:conversationId" , getMessage)
+router.get("/get-messages/:conversationId" , getMessage)
 
 
 export default router;
