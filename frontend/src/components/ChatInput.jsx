8e30b1bc35da313c00385e787a76516f84e67e38
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-30 14:18:21 +0530

    created sendmessage api and worked on ui

--- frontend/src/components/ChatInput.jsx
+++ frontend/src/components/ChatInput.jsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ChatInput = () => {
+
+  const [value, setvalue] = useState("");
+
   return (
     <div>
       
--- frontend/src/components/MessageList.jsx
+++ frontend/src/components/MessageList.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import logo from '../assets/logo.jpg';
 
 const MessageList = () => {
   const { selectedConversation } = useSelector(state => state.conversation)
   const { messages } = useSelector(state => state.message)
   return (
-    <div>
-
+    <div className="flex-1 overflow-y-auto relative p-4 flex flex-col gap-2">
+      <div 
+        className="absolute inset-0 z-0 opacity-10 pointer-events-none flex justify-center items-center"
+        style={{ backgroundImage: `url(${logo})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'center', backgroundSize: 'contain' }}
+      >
+      </div>
+      <div className="relative z-10 w-full h-full">
+        {/* Messages will go here */}
+      </div>
     </div>
   );
 }
