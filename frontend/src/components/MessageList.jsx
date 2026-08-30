f74238d0cefd273a6f10dbbcb69103a02afd2aef
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-30 14:11:29 +0530

    working on ui

--- frontend/src/components/MessageList.jsx
+++ frontend/src/components/MessageList.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const MessageList = () => {
+  const { selectedConversation } = useSelector(state => state.conversation)
+  const { messages } = useSelector(state => state.message)
   return (
     <div>
-      
+
     </div>
   );
 }
