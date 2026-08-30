6be0aa7ee4c0b6e881fdbad4e7396bd51dcaced5
Vipul Gupta <vipulgupta92998@gmail.com>
2026-08-30 14:09:39 +0530

    chat area work done now working on nav

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 const Nav = () => {
+    const { selectedConversation } = useSelector(state => state.conversation)
+    const { messages } = useSelector(state => state.message)
   return (
     <div>
       
--- frontend/src/features/getMessages.js
+++ frontend/src/features/getMessages.js
@@ -4,6 +4,7 @@ import api from '../../utils/axios.js';
 async function getMessages(id) {
     try {
         const {data} = await api.get(`/api/chat/get-messages/${id}`)
+        console.log(data)
         return data
     } catch (error) {
         console.log(error)
