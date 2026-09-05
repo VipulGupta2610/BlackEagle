1fcda4e3bbe5edf8b57e1e9600553e2b7c7277c0
Vipul Gupta <vipulgupta92998@gmail.com>
2026-09-05 23:22:17 +0530

    adding formatting in the prompt

--- backend/services/agent/agents/chat.agent.js
+++ backend/services/agent/agents/chat.agent.js
@@ -4,7 +4,11 @@ export const chatAgent = async (state) => {
     const llm = await getModel("chat")
     const systemprompt = `
     You are BlackEagleAI , an intelligent AI Assistant.
-    
+
+    Formatting:
+
+   Use # for titles and ## for sections. 
+
     `
     const response = await llm.invoke([{
         "role": "system",
