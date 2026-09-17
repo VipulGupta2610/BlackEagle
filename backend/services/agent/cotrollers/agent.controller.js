5a03e48834dd89bd46d76682a8045a552dd08e8c
Vipul Gupta <vipulgupta92998@gmail.com>
2026-09-17 12:44:24 +0530

    adding ai messages

--- backend/services/agent/cotrollers/agent.controller.js
+++ backend/services/agent/cotrollers/agent.controller.js
@@ -13,6 +13,9 @@ export const agent = async (req, res) => {
             prompt , conversationId
         })
         const response = result.aiResponse
+
+         await addMessage({conversationId,role:"assistant",response})
+
           await axios.post(`${process.env.CHAT_SERVICE}/save-message`, { conversationId: conversationId, role: "assistant", content: response })
         return res.status(200).json(response)
     } catch (error) {
