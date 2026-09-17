80fd2abdd7c8cb4b8bf3b3aa8aadd54d79e6e7d5
Vipul Gupta <vipulgupta92998@gmail.com>
2026-09-17 12:43:20 +0530

    adding user messages in memory

--- backend/services/agent/config/memory.js
+++ backend/services/agent/config/memory.js
@@ -14,7 +14,7 @@ export const getMemory = async (conversationId) => {
 }
 
 
-export const getMessage = async (conversationId,role,content)=>{
+export const addMessage = async (conversationId,role,content)=>{
     const key=`messages-${conversationId}`
     const rawMessages = await redis.get(key)
     const messages =rawMessages? JSON.parse(rawMessages):[]
--- backend/services/agent/cotrollers/agent.controller.js
+++ backend/services/agent/cotrollers/agent.controller.js
@@ -1,9 +1,13 @@
 import axios from "axios"
 import {graph} from "../graph/graph.js"
+import { addMessage } from "../config/memory.js";
 
 export const agent = async (req, res) => {
     try {
         const { prompt, conversationId } = req.body;
+
+        await addMessage({conversationId,role:"user",prompt})
+
         await axios.post(`${process.env.CHAT_SERVICE}/save-message`, { conversationId: conversationId, role: "user", content: prompt })
         const result = await graph.invoke({
             prompt , conversationId
